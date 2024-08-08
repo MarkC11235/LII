1acdb5ec1466fc00aa3e6c911496b762dfe258dd
Mark Desktop <marklouiscastle@gmail.com>
2024-08-07 22:15:45 -0500

    added descriptions above each opcode

--- src_bytecode/bytecode_generator.hpp
+++ src_bytecode/bytecode_generator.hpp
@@ -18,48 +18,214 @@
 
 enum OpCode{
     // Arithmetic
+
+    /*
+    * OP_ADD: Add the top two values on the stack
+                Pushes the result back onto the stack
+    */
     OP_ADD,
+    /*
+    * OP_SUB: Subtract the top two values on the stack
+                Pushes the result back onto the stack
+    */
     OP_SUB,
+    /*
+    * OP_U_SUB: Unary subtraction, negate the top value on the stack
+                Pushes the result back onto the stack
+    */
     OP_U_SUB,
+    /*
+    * OP_MUL: Multiply the top two values on the stack
+                Pushes the result back onto the stack
+    */
     OP_MUL,
+    /*
+    * OP_DIV: Divide the top two values on the stack
+                Pushes the result back onto the stack
+                If the second value is 0, throws a runtime error
+    */
     OP_DIV,
     // Boolean
+    /*
+    * OP_AND: Logical AND of the top two values on the stack
+                Pushes the result back onto the stack
+    */
     OP_AND,
+    /*
+    * OP_OR: Logical OR of the top two values on the stack
+                Pushes the result back onto the stack
+    */
     OP_OR,
+    /*
+    * OP_NOT: Logical NOT of the top value on the stack
+                Pushes the result back onto the stack
+    */
     OP_NOT,
+
     //Comparison
+
+    /*
+    * OP_EQ: Check if the top two values on the stack are equal
+                Pushes the result back onto the stack
+                Will type coerce unless both values are numbers or both values are strings
+    */
     OP_EQ,
+    /*
+    * OP_NEQ: Check if the top two values on the stack are not equal
+                Pushes the result back onto the stack
+                Will type coerce unless both values are numbers or both values are strings
+    */
     OP_NEQ,
+    /*
+    * OP_GT: Check if the second value on the stack is greater than the top value on the stack
+                Pushes the result back onto the stack
+                Throws a runtime error if the values are not numbers
+    */
     OP_GT,
+    /*
+    * OP_LT: Check if the second value on the stack is less than the top value on the stack
+                Pushes the result back onto the stack
+                Throws a runtime error if the values are not numbers
+    */
     OP_LT,
+    /*
+    * OP_GTEQ: Check if the second value on the stack is greater than or equal to the top value on the stack
+                Pushes the result back onto the stack
+                Throws a runtime error if the values are not numbers
+    */
     OP_GTEQ,
+    /*
+    * OP_LTEQ: Check if the second value on the stack is less than or equal to the top value on the stack
+                Pushes the result back onto the stack
+                Throws a runtime error if the values are not numbers
+    */
     OP_LTEQ,
     // Variables
-    OP_LOAD, // push a value from the constants array to the stack, index is the next byte
-    OP_STORE_VAR, // store a value from the stack to the variables map
-    OP_UPDATE_VAR, // update a value in the variables map
+
+    /*
+    * OP_LOAD: Push a value from the constants array to the stack
+                Index of the constants array is the next byte
+    */
+    OP_LOAD,
+    /*
+    * OP_STORE_VAR: Store a value from the stack to the variables map
+                Index of the variable name in the variable names array is the next byte
+    */
+    OP_STORE_VAR, 
+    /*
+    * OP_UPDATE_VAR: Update a value in the variables map with a value from the stack
+                Index of the variable name in the variable names array is the next byte
+    */
+    OP_UPDATE_VAR, 
+    /*
+    * OP_LOAD_VAR: Push a value from the variables map to the stack
+                Index of the variable name in the variable names array is the next byte
+    */
     OP_LOAD_VAR, // push a value from the variables map to the stack
-    OP_LOAD_FUNCTION_VAR, // looks up the function frames to find function variable
+    /*
+    * OP_LOAD_FUNCTION_VAR: Looks up the function frames to find the function variable
+                Index of the variable name in the variable names array is the next byte
+    */
+    OP_LOAD_FUNCTION_VAR, 
+
     // Arrays
-    OP_CREATE_VECTOR, // make empty vector, needs to be followed by an index to store the vector in the variables map
-    OP_VECTOR_PUSH, // insert value from stack into vector, needs to be followed by the index of the vector in the variables map
-    OP_LOAD_VECTOR_ELEMENT, // followed by the index of the vector in the variables map, and the index into the vector
-    OP_UPDATE_VECTOR_ELEMENT, // insert value from stack, followed by the index of the vector in the variables map, the index into the vector
+
+    /*
+    * OP_CREATE_VECTOR: Make an empty vector
+                Index of the vector in the variable names array is the next byte
+                Put the vector in the variables map
+    */
+    OP_CREATE_VECTOR, 
+    /*
+    * OP_VECTOR_PUSH: Push a value onto the vector
+                Index of the vector in the variable names array is the next byte
+    */
+    OP_VECTOR_PUSH, 
+    /*
+    * OP_LOAD_VECTOR_ELEMENT: Load a value from the vector
+                Index of the vector in the variable names array is the next byte
+                The index of the value in the vector is on the stack
+    */
+    OP_LOAD_VECTOR_ELEMENT,
+    /*
+    * OP_UPDATE_VECTOR_ELEMENT: Update a value in the vector
+                Index of the vector in the variable names array is the next byte
+                The index of the value in the vector is on the stack
+                The value to update is on the stack
+    */
+    OP_UPDATE_VECTOR_ELEMENT,
+
     // Structs
-    OP_CREATE_STRUCT, // make empty struct, needs to be followed by an index to store the struct in the variables map
-    OP_LOAD_STRUCT_ELEMENT, // followed by the index of the struct in the variables map, and the index into the struct
-    OP_UPDATE_STRUCT_ELEMENT, // insert value from stack, followed by the index of the struct in the variables map
+
+    /*
+    * OP_CREATE_STRUCT: Make an empty struct
+                Index of the struct in the variable names array is the next byte
+                Put the struct in the variables map
+    */
+    OP_CREATE_STRUCT, 
+    /*
+    * OP_LOAD_STRUCT_ELEMENT: Load a value from the struct and push it onto the stack
+                Index of the struct in the variable names array is the next byte
+                Name of the element in the struct is the next byte
+    */
+    OP_LOAD_STRUCT_ELEMENT, 
+    /*
+    * OP_UPDATE_STRUCT_ELEMENT: Update a value in the struct
+                Index of the struct in the variable names array is the next byte
+                Name of the element in the struct is the next byte
+                The value to update is on the stack
+    */
+    OP_UPDATE_STRUCT_ELEMENT, 
+
     // Control flow
+
+    /*
+    * OP_RETURN: Return the top value on the stack
+                Pops the value off the stack
+    */
     OP_RETURN,
+    /*
+    * OP_JUMP: Jump to a different part of the code
+                Offset is the next byte
+    */
     OP_JUMP,
+    /*
+    * OP_JUMP_IF_FALSE: Jump to a different part of the code if the top value on the stack is false
+                Offset is the next byte
+    */
     OP_JUMP_IF_FALSE,
+    /*
+    * OP_FUNCTION_CALL: Call a function
+                The function is the top value on the stack
+                The arguments are below the function on the stack
+    */
     OP_FUNCTION_CALL,
-    //Scope 
+
+    //Scope
+
+    /*
+    * OP_INC_SCOPE: Increase the scope level by 1
+    */ 
     OP_INC_SCOPE,
+    /*
+    * OP_DEC_SCOPE: Decrease the scope level by 1
+    */
     OP_DEC_SCOPE,
+
     // Output
+    /*
+    * OP_PRINT: Print the top value on the stack
+                Pops the value off the stack
+    */
     OP_PRINT,
+
     // Stdlib
+
+    /*
+    * OP_STD_LIB_CALL: Call a function from the standard library
+                Index of the function in the standard library functions array is the next byte
+                The args are on the stack
+    */
     OP_STD_LIB_CALL
 };
 
@@ -83,10 +249,6 @@ std::unordered_map<std::string, OpCode> opCodeMap = {
 
 
 // Data Structures ---------------------------------------------------
-
-//std::map<std::string, function*> function_definitions; // Dynamically allocated because multiple functions can be created
-                                                        // This vector stores the functions
-
 
 // Constants struct
 // This is so constants can be stored in the bytecode as indices
@@ -420,36 +582,10 @@ void interpret_function_call(Node* node, function* func){
     //get the name of the function
     std::string name = node->get_value(1);
 
-    //look up the function scopes to find the function
-    //finds definition that is closest to the current function
-    // function* func_where_called_func_resides = func;
-    // while(func_where_called_func_resides != nullptr){
-    //     if(func_where_called_func_resides->functions.find(name) != func_where_called_func_resides->functions.end()){
-    //         break;
-    //     }
-    //     func_where_called_func_resides = func_where_called_func_resides->parent;
-    // }
-
-    // if(func_where_called_func_resides == nullptr){
-    //     interpretation_error("Function not found: " + name, nullptr);
-    // }
-
-    // name = name + '^' + func_where_called_func_resides->name;
-
-    // Get the function from the variables map
-    // lookup the function in the functions map
-    // function* called_func = func_where_called_func_resides->functions[name];
-    // if(called_func == nullptr){
-    //     interpretation_error("Function not found: " + name, nullptr);
-    // }
-
-    // //check if the number of arguments is correct
-    // if(node->get_child(0)->get_children().size() != called_func->arguments.size()){
-    //     interpretation_error("Incorrect number of arguments", node);
-    // }
     //push the arguments to the stack
-    for(int i = 0; i < (int)node->get_child(0)->get_children().size(); i++){
-        interpret_expr(node->get_child(0)->get_child(i), func);
+    Node* arg_list = node->get_child(0);
+    for(int i = 0; i < (int)arg_list->get_children().size(); i++){
+        interpret_expr(arg_list->get_child(i), func);
     }
 
     //Push the function onto the stack
@@ -461,11 +597,6 @@ void interpret_function_call(Node* node, function* func){
 
     //call the function
     WRITE_BYTE(OpCode::OP_FUNCTION_CALL, func);
-    //write the function index
-    // if(get_variable_index(name) == -1){
-    //     interpretation_error("Function not found: " + name, node);
-    // }
-    // WRITE_BYTE(get_variable_index(name), func);
 }
 
 void interpret_std_lib_call(Node* node, function* func){
@@ -473,23 +604,23 @@ void interpret_std_lib_call(Node* node, function* func){
         interpretation_error("Std Lib call doesn't start with STD_LIB_CALL Node", node, func);
     }
 
-    //get the name of the function
-    std::string name = node->get_value(1);
+    std::string function_name = node->get_value(1);
 
     //push the arguments to the stack
-    for(int i = 0; i < (int)node->get_child(0)->get_children().size(); i++){
-        interpret_expr(node->get_child(0)->get_child(i), func);
+    Node* arg_list = node->get_child(0);
+    for(int i = 0; i < (int)arg_list->get_children().size(); i++){
+        interpret_expr(arg_list->get_child(i), func);
     }
 
     WRITE_BYTE(OpCode::OP_STD_LIB_CALL, func);
     // look up the function in the std lib function names array
     for(int i = 0; i < (int)STD_LIB_FUNCTIONS_DEFINITIONS.size(); i++){
-        if(name == STD_LIB_FUNCTIONS_DEFINITIONS[i].name){
+        if(function_name == STD_LIB_FUNCTIONS_DEFINITIONS[i].name){
             WRITE_BYTE(i, func);
             return;
         }
     }
-    interpretation_error("Std Lib function not found: " + name, node, func);
+    interpretation_error("Std Lib function not found: " + function_name, node, func);
 }
 
 void choose_expr_operand(Node* node, function* func){
